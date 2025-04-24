c396666e70afb019c22120358fa8796f14f5102c
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-24 21:42:08 +0200

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float by multiplier."""
     return lambda x: x * multiplier
