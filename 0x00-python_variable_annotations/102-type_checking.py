fa7cf81202f7bbc3e1ec82293e4f472cc2c1075f
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-24 21:56:03 +0200

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
+'''Function zoom_array'''
 from typing import List, Tuple
 
+
 def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    """Repeat each item in a tuple a certain number of times."""
-    zoomed_in: List[int] = [
-        item for item in lst
-        for i in range(factor)
-    ]
+    '''Repeats each element in a tuple by a given factor.'''
+    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
     return zoomed_in
 
+
 array = (12, 72, 91)
-
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
