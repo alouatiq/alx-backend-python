49a85ea9cec6ebeb445d31d4b1f78e6c467ecf02
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-24 21:58:17 +0200

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,11 +4,14 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    '''Repeats each element in a tuple by a given factor.'''
-    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
-    return zoomed_in
+    '''Return a new list where each item is repeated by the factor.'''
+    return [item for item in lst for _ in range(factor)]
 
 
-array = (12, 72, 91)
-zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, 3)
+if __name__ == "__main__":
+    array = (12, 72, 91)
+    zoom_2x = zoom_array(array)
+    print(zoom_2x)  # [12, 12, 72, 72, 91, 91]
+
+    zoom_3x = zoom_array(array, 3)
+    print(zoom_3x)  # [12, 12, 12, 72, 72, 72, 91, 91, 91]
