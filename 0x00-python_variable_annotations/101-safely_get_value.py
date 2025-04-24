6bdaed00904245502f1bc86ff9c8b26c3d53f3ff
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-24 21:54:42 +0200

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+'''Function safely_get_value'''
 from typing import Mapping, Any, Union, TypeVar
+
 
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """Get value from dict or return default."""
+    '''Safely retrieves a value from a dictionary or returns a default.'''
     if key in dct:
         return dct[key]
     return default
