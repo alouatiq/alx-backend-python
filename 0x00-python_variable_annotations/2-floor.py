05190c46073d3645b9d6126a84da0c947f390843
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-24 21:50:45 +0200

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+'''Function floor'''
 import math
 
+
 def floor(n: float) -> int:
-    """Return the floor of a float."""
+    '''Returns the floor of a float.'''
     return math.floor(n)
