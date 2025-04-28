ba4f677f761c4f821c611202857b8340b1b71daa
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-28 13:23:35 +0200

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,8 @@ from typing import Union
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay up to max_delay seconds and returns the delay."""
+    """Waits for a random delay up to
+    max_delay seconds and returns the delay."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
