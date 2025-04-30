9896556a60cb4cc4317b0c6ee3709615aee55cb6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-30 09:40:29 +0200

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Async comprehension that collects 10 numbers from async_generator"""
 from typing import List
-from 0-async_generator import async_generator  # Or: from . import async_generator
+from importlib import import_module
+
+# Dynamically import the module (since its name starts with a digit and has a hyphen)
+async_generator = import_module("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
