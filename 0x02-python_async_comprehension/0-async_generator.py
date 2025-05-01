3a6f04728df581337a4b694688cd399800b904ff
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-05-01 22:07:58 +0200

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Yield a random float between 0
     and 10 every second for 10 iterations."""
     for _ in range(10):
